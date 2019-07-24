75dd383afb36ddc9a5e75f605b3f5e6443653049
sudoxx2 <sudoxx2@MacBook-Pro.local>
2019-07-23 22:21:30 -0700

    templates added no base class

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Nani',
+        'title': 'blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'omerae',
+        'title': 'blog post 2',
+        'content': 'First post content',
+        'date_posted': 'July 27, 2018'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>blog home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>about blog</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
