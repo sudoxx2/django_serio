63b955bf0bb36f182285dfa52676a40dd5634e11
sudoxx2 <sudoxx2@MacBook-Pro.local>
2019-07-24 21:40:01 -0700

    database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# Create your views here.
+from .models import Post
 
 posts = [
     {
@@ -18,7 +18,7 @@ posts = [
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
